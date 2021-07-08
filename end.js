2454ba55306483dadc535b51b1c4270e408a6ed7
hansraj95 <bittuhansa56789@gmail.com>
2021-07-08 15:06:34 +0530

    third commit

--- end.js
+++ end.js
@@ -1,5 +1,4 @@
 let a=document.getElementById("info")
 username=sessionStorage.getItem("username")
 point=sessionStorage.getItem("points")
-console.log("f c5rcercn8vfa")
 a.innerHTML=`<h3>${username} your score is ${point} out of 100</h3>`
